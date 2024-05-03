c04060f1bd65e338b5a30463cd655f9cad2a24db
vlyngaae <vlyngaae@student.unimelb.edu.au>
2024-05-03 10:00:34 +1000

    nav slide out added - css logic refined and class name changed

--- js/frame.js
+++ js/frame.js
@@ -1,13 +1,13 @@
 let menuBtn = document.getElementById("menu-btn");
 let navBar = document.querySelector("nav")
 menuBtn.addEventListener("click", function() {
-    navBar.classList.toggle("mobile-open");
+    navBar.classList.toggle("slide-in");
 }); 
 
 
 //  behaviour that occurs to reset elements upon resize
 addEventListener("resize", (event) => {
     if (window.innerWidth > 580){
-        navBar.classList.remove("mobile-open");
+        navBar.classList.remove("slide-in");
     }
 });
